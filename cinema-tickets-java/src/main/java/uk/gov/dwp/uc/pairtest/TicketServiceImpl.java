826c3792160f4c8decd96736d3ddd33e787374a1
Shaan Khokhar <shaan.khokhar@nutracheck.co.uk>
2025-09-19 19:28:23 +0100

    Task 1: Process a single adult ticket purchase with no validation

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,16 +1,28 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
-    /**
-     * Should only have private methods other than the one below.
-     */
 
-    @Override
-    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+    private final TicketPaymentService ticketPaymentService;
+    private final SeatReservationService seatReservationService;
 
+    public TicketServiceImpl(TicketPaymentService ticketPaymentService,
+                             SeatReservationService seatReservationService) {
+        this.ticketPaymentService = ticketPaymentService;
+        this.seatReservationService = seatReservationService;
     }
 
+    @Override
+    public void purchaseTickets(final Long accountId, final TicketTypeRequest... ticketTypeRequests)
+            throws InvalidPurchaseException {
+
+        // The simplest implementation to make the first test pass.
+        seatReservationService.reserveSeat(accountId, 1);
+        ticketPaymentService.makePayment(accountId, 20);
+    }
 }
+
---
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -0,0 +1,43 @@
+package uk.gov.dwp.uc.pairtest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+final class TicketServiceImplTest {
+
+    @Mock
+    private TicketPaymentService ticketPaymentService;
+
+    @Mock
+    private SeatReservationService seatReservationService;
+
+    private TicketService ticketService;
+
+    @BeforeEach
+    void setUp() {
+        ticketService = new TicketServiceImpl(ticketPaymentService, seatReservationService);
+    }
+
+    @Test
+    @DisplayName("Should process a single adult ticket purchase")
+    void shouldProcessSingleAdultTicket() {
+        final long accountId = 1L;
+        final TicketTypeRequest adultRequest = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1);
+
+        ticketService.purchaseTickets(accountId, adultRequest);
+
+        verify(seatReservationService).reserveSeat(accountId, 1);
+        verify(ticketPaymentService).makePayment(accountId, 20);
+    }
+}
+
